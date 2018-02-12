3526cc527978b0355df3f366174363090f0274c4
Caique de Castro <caiquecastro@outlook.com>
2018-02-12 11:33:58 -0200

    fix: Translate messages from vue-table-component
    Use custom settings to override default values.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -5,20 +5,19 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+import './bootstrap';
 
-window.Vue = require('vue');
+import Vue from 'vue';
 import VueTheMask from 'vue-the-mask'
 import TableComponent from 'vue-table-component';
 
 /**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
+ * Install the plugins for Vue. And attach the Vue instance to the page.
  */
-
+Vue.use(TableComponent, {
+    filterNoResults: 'Não foram encontrados resultados...',
+});
 Vue.use(VueTheMask);
-Vue.use(TableComponent);
 
 const app = new Vue({
     el: '#app'
