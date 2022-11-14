992f1ddde75f342550801637a0e9ab709175658c
Caique de Castro <castro.caique@gmail.com>
2022-11-14 10:37:45 -0300

    Fix table name on migration

--- database/migrations/2017_12_23_170720_create_researches_table.php
+++ database/migrations/2017_12_23_170720_create_researches_table.php
@@ -13,7 +13,7 @@ class CreateResearchesTable extends Migration
      */
     public function up()
     {
-        Schema::create('researches', function (Blueprint $table) {
+        Schema::create('research', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->timestamps();
@@ -27,6 +27,6 @@ class CreateResearchesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('researches');
+        Schema::dropIfExists('research');
     }
 }
